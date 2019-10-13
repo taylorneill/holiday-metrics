7cdee9bed9792ed92b0e51ad3fd3291ac6c8eb5c
Andre Lowy <andre.lowy@gmail.com>
2019-10-13 18:33:44 -0400

    added eslint airbnb settings. configured them. added cors package.

--- app.js
+++ app.js
@@ -1,64 +1,65 @@
-//setup express
-const express = require('express')
-const app = express()
-const port = 3000
+// setup express
+const express = require('express');
 
-//searchKey test
-var searchKey = "123123124";
+const app = express();
+const port = 3000;
+const cors = require('cors');
+const csv = require('csv');
 
-//let searchKey = function () {
+// searchKey test
+const searchKey = '123123124';
 
-//};
+// let searchKey = function () {
+
+// };
 
 // setup aws-sdk
-var AWS = require('aws-sdk');
-//const config = require('config/config.js');
-//const isDev = process.env.NODE_END !== 'production';
+const AWS = require('aws-sdk');
+// const config = require('config/config.js');
+// const isDev = process.env.NODE_END !== 'production';
 
-//read config.js 
+// read config.js
 
 
-let awsConfig = {
-    "region" : "us-east-1",
-    "endpoint" :"https://dynamodb.us-east-1.amazonaws.com"
+const awsConfig = {
+  region: 'us-east-1',
+  endpoint: 'https://dynamodb.us-east-1.amazonaws.com',
 };
 
 
 AWS.config.update(awsConfig);
 
-//do stuff
-let docClient = new AWS.DynamoDB.DocumentClient();
+// do stuff
+const docClient = new AWS.DynamoDB.DocumentClient();
 
-let retrieveArtistStats = function () {
-    var params = {
-        TableName: "ARTIST_DATA",
-        Key: {
-            "ARTIST_ID": searchKey
-        }
-    };
+const retrieveArtistStats = () => {
+  const params = {
+    TableName: 'ARTIST_DATA',
+    Key: {
+      ARTIST_ID: searchKey,
+    },
+  };
 
-        docClient.get(params, function(err, data) { 
-            if (err) {
-                console.log("ARTIST_DATA::retrieveArtistStats::error - " + JSON.stringify(err, null, 2));
-            }
-            else {
-               // console.log("ARTIST_DATA::retrieveArtistStats::success - " + JSON.stringify(data, null, 2));
-              return JSON.stringify(data,null,2);
-             // console.log(data);
-            }
-        }
-        );
-}
+  docClient.get(params, (err, data) => {
+    if (err) {
+    //   console.log(`ARTIST_DATA::retrieveArtistStats::error - ${JSON.stringify(err, null, 2)}`);
+      return `ARTIST_DATA::retrieveArtistStats::error - ${JSON.stringify(err, null, 2)}`;
+    }
+    // console.log("ARTIST_DATA::retrieveArtistStats::success - " + JSON.stringify(data, null, 2));
+    return JSON.stringify(data, null, 2);
+    // console.log(data);
+  });
+};
 
-let searchOutput = retrieveArtistStats();
+const searchOutput = retrieveArtistStats();
 
 
-//do stuff
+// do stuff
 
-//app.get('/', (req, res) => res.send(searchOutput))
+// app.get('/', (req, res) => res.send(searchOutput))
 
-app.get('/', function(req, res){
-    res.send('id: '+ req.query.id + ' \n\n\n' + searchOutput);
+app.get('/', (req, res) => {
+  res.send(`id: ${req.query.id} \n\n\n${searchOutput}`);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port} and ${process.env.ACCESS_KEY_ID}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port} and ${process.env.ACCESS_KEY_ID}!`));
--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
-let awsConfig = {
-    "region" : "us-east-1",
-    "endpoint" :"http://dynamodb.us-east-1.amazonaws.com",
-    "accessKeyID" : process.env.ACCESS_KEY_ID,
-    "secretAccessKey" : process.env.SECRET_ACCESS_KEY,
-};
+// const awsConfig = {
+//   region: 'us-east-1',
+//   endpoint: 'http://dynamodb.us-east-1.amazonaws.com',
+//   accessKeyID: process.env.ACCESS_KEY_ID,
+//   secretAccessKey: process.env.SECRET_ACCESS_KEY,
+// };
