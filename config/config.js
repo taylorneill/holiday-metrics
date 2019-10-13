5b983b59489e199f3edbdb41d39a93bbaba82c24
Andre Lowy <andre.lowy@gmail.com>
2019-10-13 16:58:15 -0400

    update gitignore

--- config/config.js
+++ config/config.js
@@ -2,5 +2,5 @@ let awsConfig = {
     "region" : "us-east-1",
     "endpoint" :"http://dynamodb.us-east-1.amazonaws.com",
     "accessKeyID" : "AKIATEB32ECC3NR4MD4H",
-    "secretAccessKey" : " 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,
+    "secretAccessKey" : "L7o+v15y+tWzPasZMwEmHyMxXdBIcarKY8sKID46",
 };
